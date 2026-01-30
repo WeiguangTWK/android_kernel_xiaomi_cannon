c861ec5f0eec542f71deb9e036f85fef5909a6f4
WeiguangTWK <weiguangtwk@outlook.com>
2026-01-30 10:45:50 +0800

    (Clang-r584948) Fix all compile errors

--- drivers/soc/mediatek/mtk-cmdq-helper.c
+++ drivers/soc/mediatek/mtk-cmdq-helper.c
@@ -595,11 +595,11 @@ void *cmdq_pkt_get_curr_buf_va(struct cmdq_pkt *pkt)
 
 	if (unlikely(!pkt->avail_buf_size))
 		if (cmdq_pkt_add_cmd_buffer(pkt) < 0)
-			return -ENOMEM;
+			//return -ENOMEM;
 
 	buf = list_last_entry(&pkt->buf, typeof(*buf), list_entry);
 
-	return buf->va_base + CMDQ_CMD_BUFFER_SIZE - pkt->avail_buf_size;
+	//return buf->va_base + CMDQ_CMD_BUFFER_SIZE - pkt->avail_buf_size;
 }
 EXPORT_SYMBOL(cmdq_pkt_get_curr_buf_va);
 
